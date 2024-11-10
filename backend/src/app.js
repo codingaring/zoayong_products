c1be6dad631285d3b3e790f8c439b5e12e770454
yu kyoung kim <sksms2104@naver.com>
2024-11-10 19:44:51 +0900

    Chore: BACK_mongoDB 연결 및 http 설정

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,15 @@
-console.log("Express 앱 설정 및 미들웨어 등록");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch((error) => console.error("Connection error", error));
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => console.log("Server Started"));
