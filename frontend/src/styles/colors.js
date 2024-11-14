ada11a9471b7c52fc7ae7d3cd3b3b5d4f92a3e61
yu kyoung kim <sksms2104@naver.com>
2024-11-14 19:02:10 +0900

    Style: 색상 팔레트 추가

---
+++ frontend/src/styles/colors.js
@@ -0,0 +1,32 @@
+const COLORS = {
+  black: {
+    900: '2c2c2c',
+    800: '37393F',
+  },
+  gray: {
+    500: 'c7c7c7',
+  },
+  grren: {
+    900: '4E7C59',
+    800: '79978D',
+    700: '549F78',
+    600: '62BC9F',
+    500: '8FC1A9',
+    300: 'A9D3B1',
+    200: 'B1DECF',
+    100: 'D9EAD3',
+  },
+  yellow: {
+    900: 'FFD700',
+    700: 'FFD966',
+    200: 'F1EC9B',
+  },
+  pink: {
+    700: 'F28B82',
+  },
+  blue: {
+    700: '5A7BFF',
+  },
+};
+
+export default COLORS;
