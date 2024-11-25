4c4379c51a2d27a643a741c953ee601261a3179d
yu kyoung kim <sksms2104@naver.com>
2024-11-26 00:03:44 +0900

    feat: BACK_상품 정보 get 요청 DB와 연결

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Product from "./models/Product.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
-dotenv.config({ path: ".env.local" });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env.local") });
 
 mongoose
   .connect(process.env.DATABASE_URL)
@@ -11,5 +17,33 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+// 모든 상품 조회
+/**
+ * - sort 가격을 기준으로 오름차순 내림차순
+ * - count 한 번에 몇 개 가져올 것인지
+ */
+app.get("/products", async (req, res) => {
+  const sort = req.query.sort;
+  const count = Number(req.query.count);
+
+  const sortOption = { price: sort };
+  const products = await Product.find()
+    .sort(sortOption)
+    .limit(count)
+    .select("id name price category thumbnailImage");
+
+  res.send(products);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Cannot find given id" });
+  }
+});
 
 app.listen(3000, () => console.log("Server Started"));
