a6eb7e5e04c47a5a161c91498bb9e334f9702f56
yu kyoung kim <sksms2104@naver.com>
2024-11-26 20:33:05 +0900

    feat: BACK_배포를 위한 cors 설정 추가

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import Product from "./models/Product.js";
 import { fileURLToPath } from "url";
 import path from "path";
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,6 +17,14 @@ mongoose
   .catch((error) => console.error("Connection error", error));
 
 const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+};
+
+// app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 
 function asyncHandler(handler) {
