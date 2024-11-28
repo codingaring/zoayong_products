27bc0cb7029d1cf81ae7f2ae9de9f9b917c12762
yu kyoung kim <sksms2104@naver.com>
2024-11-28 17:42:48 +0900

    feat: FRONT_Polymorphic 컴포넌트 구현

---
+++ frontend/src/components/View.tsx
@@ -0,0 +1,19 @@
+import { ComponentPropsWithoutRef, ComponentPropsWithRef, ElementType, forwardRef, Ref } from 'react';
+
+export type AsProp<T extends ElementType> = {
+  as?: T;
+};
+
+export type PolymorphicRef<T extends ElementType> = Ref<ComponentPropsWithRef<T>['ref']>;
+
+export type PolymorphicProps<T extends ElementType, Props = unknown> = AsProp<T> & ComponentPropsWithoutRef<T> & Props;
+
+const View = forwardRef(
+  <T extends ElementType = 'div'>({ as, ...props }: PolymorphicProps<T>, ref: PolymorphicRef<T>) => {
+    const Element = as || 'div';
+    return <Element ref={ref} {...props} />;
+  },
+);
+export default View;
+
+View.displayName = 'View';
