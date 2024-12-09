5a195c0d4d49900f0f70cb206d1ab888ad36dcd6
codingaring <sksms2104@naver.com>
2024-12-09 13:23:25 +0900

    chore: emotion 설정 제거

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,11 +1,8 @@
-import { Global } from '@emotion/react';
-import { globalCSS } from '@styles/globals';
 import { PropsWithChildren } from 'react';
 
 const App = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <Global styles={globalCSS} />
       <main>{children}</main>
     </>
   );
